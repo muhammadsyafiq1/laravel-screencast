ed8774681559259e2641fbfcaa940240dd783a5d
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2022-02-18 10:31:20 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,44 @@
+<div class="w-1/5 bg-gray-800 min-h-screen px-4 py-6 ">
+
+    <div class="mb-6">
+        <header class="font-medium text-gray-400 uppercase px-2 text-xs">
+            Main
+        </header>
+        <a href="" class="block text-gray-200 px-2 py-2">
+            Dashboard
+        </a>
+    </div>
+
+    <div class="mb-6">
+        <header class="font-medium text-gray-400 uppercase px-2 text-xs">
+            Playlist
+        </header>
+        <a href="" class="block text-gray-200 px-2 py-2">
+            Create
+        </a>
+        <a href="" class="block text-gray-200 px-2 py-2">
+            Table
+        </a>
+    </div>
+
+    <div class="mb-6">
+        <header class="font-medium text-gray-400 uppercase px-2 text-xs">
+            Category
+        </header>
+        <a href="" class="block text-gray-200 px-2 py-2">
+            Create
+        </a>
+        <a href="" class="block text-gray-200 px-2 py-2">
+            Table
+        </a>
+    </div>
+
+    <div class="mb-6">
+        <header class="font-medium text-gray-400 uppercase px-2 text-xs">
+            Users
+        </header>
+        <a href="" class="block text-gray-200 px-2 py-2">
+            Table
+        </a>
+    </div>
+</div>
